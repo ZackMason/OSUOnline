8068de9c0e0637dd0eaa86841010cf436b061bdd
Zack MAson <crazyzacke@gmail.com>
2021-04-29 01:17:13 -0700

    Simple web layout

--- server.py
+++ server.py
@@ -3,20 +3,6 @@ from connection import connect
 
 app = Flask(__name__)
 
-<<<<<<< Updated upstream
-local = True
-
-PORT = 18345
-HOST = '127.0.0.1' if local else '0.0.0.0'
-
-@app.route('/')
-def home_page():
-    res = ""
-      
-    return render_template("homepage.html", content=res)
-=======
->>>>>>> Stashed changes
-
 @app.route('/players')
 def players():
     return render_template("players.html")
@@ -44,13 +30,10 @@ def home_page():
     return render_template("homepage.html")
 
 if __name__ == '__main__':
-<<<<<<< Updated upstream
     app.run(host=HOST, port=PORT)
-=======
     local = bool(os.environ.get('LOCAL', True))
 
     PORT = int(os.environ.get('PORT', 18345))
     HOST = '127.0.0.1' if local else '0.0.0.0'
 
     app.run(host=HOST, port=PORT)
->>>>>>> Stashed changes
