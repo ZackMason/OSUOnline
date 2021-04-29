b416f4b2e10ae4aa02aec4d8b6b0fb2a5e2991f6
Zack MAson <crazyzacke@gmail.com>
2021-04-29 02:24:10 -0700

    added bootstrap and interactive dashboard

--- server.py
+++ server.py
@@ -1,27 +1,55 @@
 from flask import Flask, redirect, url_for, render_template
 from connection import connect
 
+import os
+from random import *
+import string
+
+def get_fake_entities():
+    attributes = ['id', 'name', 'description']
+    entities = [ {
+        'id': randint(0,100), 
+        'name':''.join(choice(string.ascii_letters) for i in range(randint(0,12))),
+        'description': ''.join(choice(string.ascii_letters) for i in range(randint(0,22))),
+        } for i in range(20)]
+    return attributes, entities
+
 app = Flask(__name__)
 
 @app.route('/players')
 def players():
-    return render_template("players.html")
+    attributes, entities = get_fake_entities()
+    return render_template('dashboard.html', title='Players', attributes=attributes, results=entities)
 
 @app.route('/quests')
 def quests():
-    return render_template("quests.html")
+    attributes, entities = get_fake_entities()
+    return render_template('dashboard.html', title='Quests', attributes=attributes, results=entities)
 
 @app.route('/items')
 def items():
-    return render_template("items.html")
+    attributes, entities = get_fake_entities()
+    return render_template('dashboard.html', title='Items', attributes=attributes, results=entities)
 
 @app.route('/npcs')
 def npcs():
-    return render_template("npcs.html")
+    attributes, entities = get_fake_entities()
+    return render_template('dashboard.html', title='NPCs', attributes=attributes, results=entities)
+
+@app.route('/maps')
+def maps():
+    attributes, entities = get_fake_entities()
+    return render_template('dashboard.html', title='Maps', attributes=attributes, results=entities)
+
 
 @app.route('/player/<name>')
 def login(name=None):
     return "Hello %s, how are you?" % name
+
+@app.route('/dashboard')
+def dashboard():
+    attributes, entities = get_fake_entities()
+    return render_template('dashboard.html', attributes=attributes, results=entities)
 
 @app.route('/')
 @app.route('/homepage')
@@ -30,7 +58,6 @@ def home_page():
     return render_template("homepage.html")
 
 if __name__ == '__main__':
-    app.run(host=HOST, port=PORT)
     local = bool(os.environ.get('LOCAL', True))
 
     PORT = int(os.environ.get('PORT', 18345))
