d3bb3174f35b1d85cdb864d942d7f9a97c04866b
batovski <56854166+batovski@users.noreply.github.com>
2021-05-13 15:25:30 -0700

    Added DELETE option

--- server.py
+++ server.py
@@ -14,6 +14,7 @@ Notes:
 
 from flask import Flask, redirect, url_for, render_template, request, flash, json
 from connection import *
+from dotenv import load_dotenv, find_dotenv
 
 import os
 from random import *
@@ -22,10 +23,11 @@ import string
 import secrets
 
 secret_key = secrets.token_hex(16)
-
+load_dotenv('./.env')
 login_name = str(os.environ.get('LOGIN', 'your_login'))
 login_pswd = str(os.environ.get('DB_PASS', 'your_password'))
 login_db = str(os.environ.get('DATABASE', 'login_again'))
+
 
 def get_entities():
     attributes = ['ID', 'name', 'description']
@@ -37,7 +39,7 @@ def get_entities():
             entities = [{
                 'ID': res[i]["player_id"],
                 'name': res[i]["name"],
-                'description':res[i]["email"],
+                'description': res[i]["email"],
             } for i in range(len(res))]
             return attributes, entities
 
@@ -118,7 +120,7 @@ def get_npcs():
             res = json.loads(results)
             entities = [{
                 'ID': res[i]["npc_id"],
-                'name':res[i]["name"],
+                'name': res[i]["name"],
                 'description': res[i]["description"],
                 'faction': res[i]["faction"],
             } for i in range(len(res))]
@@ -129,15 +131,16 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = secret_key
 
-def handle_post_request(request, table = 'df', attr = 'df'):
+
+def handle_post_request(request, table='df', attr='df', id=None):
     if request.method == 'POST':
         print('post recieved')
         if not request.form.get('query_type'):
             table_token = '%s %s' % (table, tuple(attr))
-            table_token = table_token.replace("'",'')
+            table_token = table_token.replace("'", '')
             data = [request.form.get(a) for a in attr]
             values_token = '%s' % data
-            values_token = values_token.replace("'",'').replace('[', '(').replace(']', ')')
+            values_token = values_token.replace("'", '').replace('[', '(').replace(']', ')')
             if '' in data:
                 print('empty field')
             else:
@@ -149,88 +152,136 @@ def handle_post_request(request, table = 'df', attr = 'df'):
             flash('Update request: %s ' % str(request.form))
         elif request.form.get('query_type') == 'DELETE':
             print('delete recieved')
-            flash('Delete request: %s ' % str(request.form))
+            query = "DELETE FROM %s WHERE %s == %s;" % (table, id, request.form.get('id'))
+            print(query)
+
 
 @app.route('/players', methods=['GET', 'POST'])
 def players():
     attributes, entities = get_players()
 
-    handle_post_request(request, table='player', attr=attributes)
+    handle_post_request(request, table='player', attr=attributes, id='player_id')
 
-    return render_template('players.html', title='Players', attributes=attributes, results=entities, form_attributes=attributes)
+    return render_template('players.html', title='Players', attributes=attributes, results=entities,
+                           form_attributes=attributes)
 
 
 @app.route('/quests', methods=['GET', 'POST'])
 def quests():
     attributes, entities = get_quests()
 
-    handle_post_request(request, table='quest', attr=attributes )
-    return render_template('quests.html', title='Quests', attributes=attributes, results=entities, form_attributes=attributes)
+    handle_post_request(request, table='quest', attr=attributes, id='quest_id')
+    return render_template('quests.html', title='Quests', attributes=attributes, results=entities,
+                           form_attributes=attributes)
 
 
 @app.route('/items', methods=['GET', 'POST'])
 def items():
     attributes, entities = get_items()
 
-    handle_post_request(request, table='items', attr=attributes )
+    handle_post_request(request, table='items', attr=attributes, id='item_id')
 
-    return render_template('items.html', title='Items', attributes=attributes, results=entities, form_attributes=attributes)
+    return render_template('items.html', title='Items', attributes=attributes, results=entities,
+                           form_attributes=attributes)
 
 
 @app.route('/npcs', methods=['GET', 'POST'])
 def npcs():
     attributes, entities = get_npcs()
 
-    handle_post_request(request, table='npc', attr=attributes )
+    handle_post_request(request, table='npc', attr=attributes, id='npc_id')
 
-    return render_template('npcs.html', title='NPCs', attributes=attributes, results=entities, form_attributes=attributes)
+    return render_template('npcs.html', title='NPCs', attributes=attributes, results=entities,
+                           form_attributes=attributes)
 
 
 @app.route('/maps', methods=['GET', 'POST'])
 def maps():
     attributes, entities = get_maps()
 
-    handle_post_request(request, table='map', attr=attributes )
+    handle_post_request(request, table='map', attr=attributes, id='map_id')
 
-    return render_template('maps.html', title='Maps', attributes=attributes, results=entities, form_attributes=attributes)
+    return render_template('maps.html', title='Maps', attributes=attributes, results=entities,
+                           form_attributes=attributes)
 
 
-@app.route('/player_inventory', methods=['GET', 'POST'])
+@app.route('/player_inventory')
 def player_inventory():
     attributes = ['pid', 'ItemID']
-    entities = [{'pid': randrange(0, 100), 'ItemID': randrange(0, 100)} for i in range(20)]
+    query = "SELECT * FROM player_inventory;"
+    with connect(login_name, login_pswd, login_db) as connection:
+        with execute_query(connection, query) as cursor:
+            results = json.dumps(cursor.fetchall())
+            res = json.loads(results)
+            entities = [{'pid': res[i]["player_id"],
+                         'ItemID': res[i]["item_id"]
+                         } for i in range(len(res))]
 
-    handle_post_request(request)
+    if request.method == 'POST':
+        pass
+    elif request.method == 'GET':
+        pass
 
     return render_template('player_inventory.html', title='Player Inventory', attributes=attributes, results=entities)
 
 
-@app.route('/player_quests', methods=['GET', 'POST'])
+@app.route('/player_quests')
 def player_quests():
     attributes = ['pid', 'qid']
-    entities = [{'pid': randrange(0, 100), 'qid': randrange(0, 100)} for i in range(20)]
+    query = "SELECT * FROM player_active_quests;"
+    with connect(login_name, login_pswd, login_db) as connection:
+        with execute_query(connection, query) as cursor:
+            results = json.dumps(cursor.fetchall())
+            res = json.loads(results)
+            entities = [{'pid': res[i]["player_id"],
+                         'qid': res[i]["quest_id"]
+                         } for i in range(len(res))]
 
-    handle_post_request(request)
+    if request.method == 'POST':
+        flash('Post request: ' + str(request.form))
+    elif request.method == 'GET':
+        pass
 
     return render_template('player_inventory.html', title='Player Quests', attributes=attributes, results=entities)
 
 
-@app.route('/npc_droptable', methods=['GET', 'POST'])
+@app.route('/npc_droptable')
 def npc_droptable():
     attributes = ['pid', 'ItemID']
-    entities = [{'pid': randrange(0, 100), 'ItemID': randrange(0, 100)} for i in range(20)]
+    query = "SELECT * FROM npc_drop_table;"
+    with connect(login_name, login_pswd, login_db) as connection:
+        with execute_query(connection, query) as cursor:
+            results = json.dumps(cursor.fetchall())
+            res = json.loads(results)
+            entities = [{
+                'pid': res[i]["npc_id"],
+                'ItemID': res[i]["item_id"]}
+                for i in range(len(res))]
 
-    handle_post_request(request)
+    if request.method == 'POST':
+        flash('Post request: ' + str(request.form))
+    elif request.method == 'GET':
+        pass
 
     return render_template('player_inventory.html', title='NPC Drop Table', attributes=attributes, results=entities)
 
 
-@app.route('/npc_spawned', methods=['GET', 'POST'])
+@app.route('/npc_spawned')
 def npc_spawned():
     attributes = ['pid', 'mid']
-    entities = [{'pid': randrange(0, 100), 'mid': randrange(0, 100)} for i in range(20)]
+    query = "SELECT * FROM spawned_npc;"
+    with connect(login_name, login_pswd, login_db) as connection:
+        with execute_query(connection, query) as cursor:
+            results = json.dumps(cursor.fetchall())
+            res = json.loads(results)
+            entities = [{'pid': res[i]["npc_id"],
+                         'mid': res[i]["map_id"]
+                         } for i in range(len(res))]
 
-    handle_post_request(request)
+    if request.method == 'POST':
+        flash('Post request: ' + str(request.form))
+    elif request.method == 'GET':
+        pass
 
     return render_template('player_inventory.html', title='NPCs Spawned', attributes=attributes, results=entities)
 
@@ -250,5 +301,5 @@ if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 18345))
     HOST = '127.0.0.1' if local else '0.0.0.0'
 
-    #connection = connect.connect(login_name, login_pswd, login_db)
+    # connection = connect.connect(login_name, login_pswd, login_db)
     app.run(host=HOST, port=PORT)
